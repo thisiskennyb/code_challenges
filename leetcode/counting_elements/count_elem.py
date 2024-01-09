81ac2b756e1381e6f8f94bc11bdcd083c248f5e0
Ken Bonnette <117300139+thisiskennyb@users.noreply.github.com>
2024-01-09 09:08:01 -0500

    revised counting numbers

--- leetcode/counting_elements/count_elem.py
+++ leetcode/counting_elements/count_elem.py
@@ -1,14 +1,10 @@
 def count_elem(arr):
-    dict = {}
-
-    for num in arr:
-        if not dict[num]:
-            dict[num] = 1
-        else:
-            dict[num] += 1
-
-    print(dict)
-
+    hash_set = set(arr)
+    count = 0
+    for x in arr:
+        if x + 1 in hash_set:
+            count += 1
+    return count
 
 num_list = [1,2,3]
 print(count_elem(num_list))
